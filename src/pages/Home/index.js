fd4ed95f435cf246a0e51957277f82195b65f39e
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-07-30 11:59:58 -0300

    adicionar descrição do vídeo da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ function Home() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Oi, gente! Eu sou Gabi Oliveira e esse vídeo busca ampliar o debate sobre combate às fake news e foi feito em parceria com o Avaaz."}
       />
 
       <Carousel
