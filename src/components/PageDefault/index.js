fd290ef187a1d9ba447e39d4afa168f37312998e
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-04 23:31:06 -0300

    última aula da imersão

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import Menu from '../Menu';
+import styled, { css } from 'styled-components';
+import Menu from '../Menu'
 import Footer from '../Footer';
-import styled from 'styled-components';
 
 const Main = styled.main`
-  background-color: var(--black);
-  color: var(--white);
-  flex: 1;
-  padding-top: 50px;
-  padding-left: 5%;
-  padding-right: 5%;
+background-color: var(--black);
+color: var(--white);
+flex: 1;
+padding-top: 50px;
+padding-left: 5%;
+padding-right: 5%;
+${({paddingAll})=> css`
+  padding: ${paddingAll};
+`}
 `;
 
-
-function PageDefault({ children }) {
-  return (
+function PageDefault({ children, paddingAll }){
+  return(
     <>
-      <Menu />
-        <Main>
-          {children}
-        </Main>
-      <Footer />
+    <Menu />
+    <Main paddingAll={paddingAll}>
+    {children}
+    </Main>
+    <Footer />
     </>
   );
 }
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,133 +1,95 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
-  const valoresIniciais = {
+    const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  };
-  const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(chave, valor) {
-    // chave: nome, descricao
-    setValues({
-      ...values,
-      [chave]: valor, // nome: 'valor'
-    });
   }
+    const {handleChange, values, clearForm}= useForm(valoresIniciais);
+    const[categorias, setCategorias] = useState([]);
 
-  function handleChange(infosDoEvento) {
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value,
-    );
-  }
-
-  // ============
-
-  useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'https://blackflix.herokuapp.com/categorias';
-      fetch(URL)
-        .then(async (respostaDoServer) => {
-          if (respostaDoServer.ok) {
-            const resposta = await respostaDoServer.json();
-            setCategorias(resposta);
-            return;
-          }
-          throw new Error('Não foi possível pegar os dados');
-        });
-    }
-  }, []);
-
-  return (
-    <PageDefault>
-      <h1>
-        Cadastro de Categoria:
-        {values.nome}
-      </h1>
-
-      <form onSubmit={function handleSubmit(infosDoEvento) {
-        infosDoEvento.preventDefault();
-
+    useEffect(() =>{
+      const URL_BACKEND = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://blackflix.herokuapp.com/categorias';
+      fetch(URL_BACKEND)
+      .then(async(respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
         setCategorias([
-          ...categorias,
-          values,
+          ...resposta,
         ]);
+      });
+    });
 
-        setValues(valoresIniciais);
-      }}
-      >
+    return ( 
+        <PageDefault>
+        <h1> Cadastro de Categoria: {values.nome}</h1>
+
+        <form onSubmit={function handleSubmit(infosDoEvento){
+          infosDoEvento.preventDefault();
+            setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          clearForm();
+        }}>
 
         <FormField
           label="Nome da Categoria"
-          type="text"
           name="nome"
           value={values.nome}
           onChange={handleChange}
         />
 
-        <FormField
-          label="Descrição"
-          type="textarea"
-          name="descricao"
-          value={values.descricao}
-          onChange={handleChange}
-        />
-        {/* <div>
-          <label>
-            Descrição:
-            <textarea
-              type="text"
-              value={values.descricao}
-              name="descricao"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
 
-        <FormField
-          label="Cor"
-          type="color"
-          name="cor"
-          value={values.cor}
-          onChange={handleChange}
+        <FormField 
+        label="Descrição"
+        type="textarea"
+        name="descricao"
+        value={values.descricao}
+        onChange={handleChange}
         />
-        {/* <div>
-          <label>
-            Cor:
-            <input
-              type="color"
-              value={values.cor}
-              name="cor"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
+          
 
+          <FormField 
+        label="Cor"
+        type="color"
+        name="cor"
+        value={values.cor}
+        onChange={handleChange}
+        />
+        
         <Button>
           Cadastrar
         </Button>
       </form>
 
-      <ul>
-        {categorias.map((categoria) => (
-          <li key={`${categoria.id}`}>
-            {categoria.titulo}
-          </li>
-        ))}
-      </ul>
+      { categorias.length === 0  && (<div>
+        Loading...
+      </div>
+      )}
 
-      <Link to="/">
-        Ir para home
-      </Link>
-    </PageDefault>
-  );
+            <ul>
+              {categorias.map((categoria)=>(
+                    <li key={`${categoria.titulo}`}>
+                    {categoria.titulo}
+                  </li>
+              ))}
+                
+            </ul>
+      
+        <Link to="/">
+            Ir para home
+        </Link>        
+        </PageDefault>
+    )
 }
 
 export default CadastroCategoria;
